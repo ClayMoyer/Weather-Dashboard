db9a6d16ae2175b92f1304cb36f33dd9a23bffcb
Clay_MM <clay.moyer.1515@gmail.com>
2023-08-01 12:35:02 -0400

    variables initiated

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const apiKey = 383dabcf23e380ff8582e432490f5e25
+let today = dayjs()
+let currentCity = document.querySelector("#currentCity")
+let temp = document.querySelector("#temperature")
+let wind = document.querySelector("#wind")
+let humidity = document.querySelector("#humidity")
+let inputSearch = document.querySelector("#inputSearch")
+let 
