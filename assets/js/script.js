7bd86aed54772fe6e139c8e6420dcf21c3faeda6
Clay_MM <clay.moyer.1515@gmail.com>
2023-08-01 13:33:15 -0400

    current weather conditions api fetch, data handling/assigning, adding proper text to label the data, pasting the appropriate weather icon, etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,22 @@ let searchHistoryDisplayed = document.querySelector("#searchHistoryDisplayed")
 let searchHistoryArr = []
 
 function weatherCurrentCity(city){
-
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey)
+    .then(response => response.json())
+    .then(data => {
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&appid=" + apiKey)
+        .then(response => response.json())
+        .then(data => {
+            saveSearchHistory(data.name)
+            let weatherIcon = document.createElement("img")
+            currentCity.textContent = data.name + " (" + today.format("MM/DD/YYYY") + ")"
+            temperature.textContent = "Temp: " + data.main.temp + "°F"
+            wind.textContent = "Wind: " + data.wind.speed + "MPH"
+            humidity.textContent = "Humidity: " + data.main.humidity + "%"
+            weatherIcon.scr = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+            currentCity.append(weatherIcon)
+        })
+    })
 }
 
 function weatherFiveDayForecast(city) {
@@ -30,5 +45,5 @@ function historyBtn() {
 }
 
 function searchCity() {
-    
+
 }
